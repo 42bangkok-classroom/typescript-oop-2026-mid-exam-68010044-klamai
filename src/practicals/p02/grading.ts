c39587cf8f966613bc02f5d2e3e56eedb927dcab
Klamai Ruasakon <68010044@kmtl.ac.th>
2026-01-21 16:29:21 +0700

    p02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,26 @@
 // Write your code below
+let values: string[] = process.argv.slice(2);
+let score: number[] = values.map(n => Number(n));
+  isNaN(number){
+ if true console.log(" Invalit input ");
+    console.log(" Invalit input ");
+    if else {
+        if (number> 100) {
+            console.log("nvalit input");
+        }
+        else if (number >= 80 && number <= 100) {
+            console.log("Your grade is A.");
+        } else if (number >= 70 && number < 80) {
+            console.log("Your grade is B.");
+        } else if (number >= 60 && number < 70) {
+            console.log("Your grade is C.");
+        } else if (number >= 50 && number < 60) {
+            console.log("Your grade is D.");
+        } else {
+            console.log("Your grade is F.");
+        }
+
+  
+};
+else {console.log("Invalit input");
+};
